73d6f54dbfde43f8270a9b9f12ca1bc22cb45d86
ryantuller.flowhub <ryan.tuller@flowhub.com>
2021-11-16 08:34:07 -0500

    Implement seeds

--- api/mysql/seed.sql
+++ api/mysql/seed.sql
@@ -1,2 +1,9 @@
 USE code_test;
 
+/* Insert test data */
+INSERT INTO results (bib, name, time)
+VALUES ('123', 'john doe', 010203);
+
+INSERT INTO results (bib, name, time)
+VALUES ('321', 'jane doe', 010101);
+
