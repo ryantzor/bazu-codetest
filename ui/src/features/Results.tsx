aaab5c7d551845bf2cd898778a6e5890a57bb4ac
ryantzor <ryantulleris@gmail.com>
2021-11-16 17:43:11 -0500

    Implement fetched ranked results from API, using hooks exclusively to manage Results' state

--- ui/src/features/Results.tsx
+++ ui/src/features/Results.tsx
@@ -1,10 +1,37 @@
-import React from "react"
-import { useAppSelector } from "../app/hooks";
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useEffect, useState } from "react"
 import { toHhMmSs } from "../time";
-import { selectResults } from "./resultsSlice";
+import { RankedResult } from "./types";
 
 export default () => {
-  const results = useAppSelector(selectResults);
+  const [results, setResults] = useState<RankedResult[]>()
+  useEffect(() => {
+    const getResults = async () => {
+      const response = await fetch(`http://localhost:5000/results`)
+      const data = await response.json() as Array<RankedResult>
+      setResults(data)
+    }
+    getResults()
+  }, [])
+
+  const ResultsView = () => {
+    if (results) {
+      return (
+      <>
+      {results?.map(x => (
+        <tr key={x.bib}>
+          <td data-testclass='bib'>{x.bib}</td>
+          <td data-testclass='name'>{x.name}</td>
+          <td data-testclass='time'>{toHhMmSs(x.time)}</td>
+          <td data-testclass='rank'>{x.rank}</td>
+        </tr>
+      ))}
+      </>
+      )
+    } else {
+      return <tr>no data</tr>
+    }
+  }
 
   return (
     <div className='results' data-testid='results'>
@@ -18,14 +45,7 @@ export default () => {
           </tr>
         </thead>
         <tbody>
-          {results.data.map(x => (
-            <tr key={x.bib}>
-              <td data-testclass='bib'>{x.bib}</td>
-              <td data-testclass='name'>{x.name}</td>
-              <td data-testclass='time'>{toHhMmSs(x.time)}</td>
-              <td data-testclass='rank'>{x.rank}</td>
-            </tr>
-          ))}
+          <ResultsView />
         </tbody>
       </table>
     </div>
